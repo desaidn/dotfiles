6d2a597faf3b172c0edc4b16964a5bc3552c1038
Dhairya Desai <me@desaidn.dev>
2026-07-16 21:07:41 -0400

    Remove stale comment

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,6 +1,1 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-
 require('custom.lib.plugins_loader').require_dir 'custom.plugins'
