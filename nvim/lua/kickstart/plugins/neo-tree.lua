bebb1b387c6c97f762cf3aa11d7f37d59142ba23
Dhairya Desai <me@desaidn.dev>
2026-08-13 18:10:29 -0400

    Constrain Neo-tree to v3

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -4,7 +4,7 @@
 local gh = require('custom.lib.pack').gh
 
 vim.pack.add {
-  { src = gh 'nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+  { src = gh 'nvim-neo-tree/neo-tree.nvim', version = vim.version.range '3' },
   gh 'nvim-lua/plenary.nvim',
   gh 'MunifTanjim/nui.nvim',
 }
